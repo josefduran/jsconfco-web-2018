82d98ebe98bbc1d199c845bd1fd9a09d4ca4144d
Jeny Alejandra Mazo <yenymzo@gmail.com>
2018-10-11 21:53:54 -0500

    adding organizers section

--- js/custom.js
+++ js/custom.js
@@ -2,13 +2,24 @@
 
 $(document).ready(changeColor);
 $(window).on('hashchange', changeColor)
+$('html,body').on('DOMMouseScroll mousewheel scroll touchmove', function (event) {
+  var scroll = $('.slide.selected .content').scrollTop();
+  console.log('sara', scroll);
+  if (scroll >= 200) {
+    $("nav.top").removeClass("transparent").addClass("blue-panel");
+  } else {
+    $("nav.top").removeClass("blue-panel").addClass("transparent");;
+  }
+});
 
 function changeColor() {
   var hash = window.location.hash;
   switch (hash) {
     case '#speakers':
     case '#schedule':
+    case '#organizers':
       $('ul.menu').css('color', '#000');
+      $('blue-panel').css('background', '#ea5c5c8f')
       break;
     default:
       $('ul.menu').css('color', '#fff');
